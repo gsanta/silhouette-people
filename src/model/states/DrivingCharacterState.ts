93125334a62d6b456a5be708d23119574796f3e1
gsanta <santagergely90@gmail.com>
2021-03-24 00:55:36 +0100

    Bike phisycs.

--- src/model/states/DrivingCharacterState.ts
+++ src/model/states/DrivingCharacterState.ts
@@ -1,21 +1,39 @@
-import { Vector2 } from "babylonjs/Maths/math.vector";
+import { Vector2 } from "babylonjs";
 import { GameObj } from "../objs/GameObj";
 
 
 export class DrivingCharacterState {
-    private traction = new Vector2(1, 1);
+    private torque = new Vector2(1, 1);
     private dragConst = new Vector2(0.2, 0.2);
+    private massVector = new Vector2(10, 10);
     private mass = 10;
     private velocity: Vector2 = new Vector2(0, 0);
+    private wheelRadius = 0.3;
+    private gearRatio = 1;
+
+    private gearRatioVec: Vector2;
+    private wheelRadiusVec: Vector2;
+    private rpm = 60;
 
     private gameObj: GameObj;
 
     constructor(gameObj: GameObj) {
         this.gameObj = gameObj;
+
+        this.gearRatioVec = this.toVec(this.gearRatio);
+        this.wheelRadiusVec = this.toVec(this.wheelRadius);
+    }
+
+    getVelocity(velocity: Vector2, deltaTime: number): Vector2 {
+        this.velocity = velocity;
+        const acceleration = this.getForce().divide(this.massVector);
+        const timeVec = new Vector2(deltaTime, deltaTime);
+
+        return velocity.add(acceleration.multiply(timeVec));
     }
 
     private getForce(): Vector2 {
-        const tractiveForce = this.getTractiveForce();
+        const tractiveForce = this.getForwardForce();
         const dragForce = this.getDragForce();
 
         return tractiveForce.add(dragForce);
@@ -27,7 +45,11 @@ export class DrivingCharacterState {
         return this.velocity.multiply(this.dragConst).multiply(speedVec).negate();
     }
 
-    private getTractiveForce(): Vector2 {
-        return this.gameObj.getFrontDirection2D().multiply(this.traction);
+    private getForwardForce(): Vector2 {
+        return this.gameObj.getFrontDirection2D().multiply(this.torque).multiply(this.gearRatioVec).divide(this.wheelRadiusVec);
+    }
+
+    private toVec(num: number) {
+        return new Vector2(num, num);
     }
 }
